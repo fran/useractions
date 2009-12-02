991835216193f65150bcbd988eab2a1deb979b3d
Francisco José Seva mora <franciscojseva@gmail.com>
2009-12-03 00:11:53 +0100

    primer commit del miniproyecto

---
+++ useractions_block_actions_autenticado.tpl.php
@@ -0,0 +1,11 @@
+<h1>useractions_block_actions_autenticado.tpl.php en theme/garalandf</h1>
+<?php
+  /**
+  * Variables in this template:
+  * $items:
+  *         $items['links']:  Array with all the links to render in the block
+  *         $items['info']: User information
+  *              $items['name']: Link to the user profile
+  *              $items['image']: User's image
+  **/
+?>
