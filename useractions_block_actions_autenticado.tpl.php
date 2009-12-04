7073fa46d7e496c83c4563f375f80cfbfc507662
Francisco José Seva mora <franciscojseva@gmail.com>
2009-12-04 10:07:45 +0100

    user actions menu, template V0.1

--- useractions_block_actions_autenticado.tpl.php
+++ useractions_block_actions_autenticado.tpl.php
@@ -1,4 +1,4 @@
-<h1>useractions_block_actions_autenticado.tpl.php en theme/garalandf</h1>
+<!--<span name="useractions_block_actions_autenticado.tpl.php en theme/garalandf"></span>-->
 <?php
   /**
   * Variables in this template:
@@ -8,4 +8,20 @@
   *              $items['name']: Link to the user profile
   *              $items['image']: User's image
   **/
+  
 ?>
+<!--User block -->
+<div>
+  <?php echo $items['info']['name'];?>
+  <img src="<?php echo $items['info']['image'];?>"/>
+</div>
+
+<!--Actions block -->
+<div>
+    <ul>
+    <?php foreach($items['links'] as $link){?>
+        <li><?php print($link); ?></li>
+    <?php }?>
+    </ul>
+</div>
+
